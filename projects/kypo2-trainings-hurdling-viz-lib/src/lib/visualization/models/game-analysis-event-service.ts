a130412e05e021f191c6f35d3548bab5663268a6
Hai Duong Tran <445437@mail.muni.cz>
2018-11-14 15:50:26 +0100

    Add interface for game analysis event service component.

---
+++ projects/kypo2-trainings-hurdling-viz-lib/src/lib/visualization/models/game-analysis-event-service.ts
@@ -0,0 +1,10 @@
+import { GameAnalysisComponent } from '../components/game-analysis/game-analysis.component';
+
+export interface GameAnalysisEventService {
+
+    GameAnalysisComponent: GameAnalysisComponent;
+
+    gameAnalysisOnBarMouseover(playerId: number): void;
+    gameAnalysisOnBarMouseout(playerId: number): void;
+    registerGameAnalysisComponent(component: GameAnalysisComponent): void;
+}
