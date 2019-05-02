1b23173428460a3aeb2af6a464f5269940d69529
xhasak <456221@mail.muni.cz>
2019-05-02 15:55:12 +0200

    added ConfigService file

--- projects/kypo2-trainings-hurdling-viz-lib/src/lib/visualization/config/config.service.ts
+++ projects/kypo2-trainings-hurdling-viz-lib/src/lib/visualization/config/config.service.ts
@@ -1,15 +1,15 @@
 import {Injectable} from '@angular/core';
-import {Kypo2TrainingsVisualizationOverviewLibConfig} from './kypo2-trainings-visualization-overview-lib';
+import {Kypo2TrainingsHurdlingVizLibConfig} from './kypo2-trainings-hurdling-viz-lib';
 
 @Injectable()
 export class ConfigService {
-  private readonly _config: Kypo2TrainingsVisualizationOverviewLibConfig;
+  private readonly _config: Kypo2TrainingsHurdlingVizLibConfig;
 
-  get config(): Kypo2TrainingsVisualizationOverviewLibConfig {
+  get config(): Kypo2TrainingsHurdlingVizLibConfig {
     return this._config;
   }
 
-  constructor(config: Kypo2TrainingsVisualizationOverviewLibConfig) {
+  constructor(config: Kypo2TrainingsHurdlingVizLibConfig) {
     this._config = config;
   }
 }
