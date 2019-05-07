f37d31f0aa8a7efb60b5e58e7f558ac2fd2363c4
xhasak <456221@mail.muni.cz>
2019-05-07 15:58:51 +0200

    added HurdlingOverviewComponent

--- src/app/hurdling/hurdling-routing.module.ts
+++ src/app/hurdling/hurdling-routing.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {VisualizationOverviewComponent} from './visualization-overview.component';
+import {HurdlingOverviewComponent} from './hurdling-overview.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: VisualizationOverviewComponent,
+    component: HurdlingOverviewComponent,
   }
 ];
 
@@ -13,6 +13,6 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class VisualizationRoutingModule {
+export class HurdlingRoutingModule {
 
 }
