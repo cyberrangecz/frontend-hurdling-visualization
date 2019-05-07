eafa4f57410dbcaae8411d7a6c0724f03d1bc8d4
xhasak <456221@mail.muni.cz>
2019-05-07 16:02:42 +0200

    added kypo2TrainingsHurdlingRestBasePath

--- src/app/custom-config.ts
+++ src/app/custom-config.ts
@@ -1,8 +1,6 @@
 import {environment} from '../environments/environment';
-import {Kypo2TrainingsVisualizationOverviewLibConfig} from '../../projects/kypo2-trainings-visualization-overview-lib/src/public_api';
+import {Kypo2TrainingsHurdlingVizLibConfig} from '../../projects/kypo2-trainings-hurdling-viz-lib/src/public_api';
 
-export const CustomConfig: Kypo2TrainingsVisualizationOverviewLibConfig = {
-  kypo2TrainingsVisualizationRestBasePath: environment.kypo2TrainingsVisualizationRestBasePath,
-  trainingInformationUrl: environment.baseUrl + '/training-definitions/4',
-  trainingEventsUrl: environment.baseUrl + '/training-events/training-definitions/4/training-instances/5'
+export const CustomConfig: Kypo2TrainingsHurdlingVizLibConfig = {
+  kypo2TrainingsHurdlingRestBasePath: environment.kypo2TrainingsHurdlingRestBasePath
 };
