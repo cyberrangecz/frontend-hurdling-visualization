a11b62385536d992e716c1174827b958f5f2b3e7
xhasak <456221@mail.muni.cz>
2019-05-15 16:03:40 +0200

    added View export

--- projects/kypo2-trainings-hurdling-viz-lib/src/public_api.ts
+++ projects/kypo2-trainings-hurdling-viz-lib/src/public_api.ts
@@ -7,3 +7,4 @@ export {Kypo2TrainingsHurdlingVizLibComponent} from './lib/kypo2-trainings-hurdl
 export {Kypo2TrainingsHurdlingVizLibModule} from './lib/kypo2-trainings-hurdling-viz-lib.module';
 export {GameAnalysisComponent} from './lib/visualization/components/game-analysis/game-analysis.component';
 export {Kypo2TrainingsHurdlingVizLibConfig} from './lib/visualization/config/kypo2-trainings-hurdling-viz-lib';
+export {View} from './lib/visualization/models/view.enum';
