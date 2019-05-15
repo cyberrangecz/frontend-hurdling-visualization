a6ef02c6862e253670c5f8c8d8be730a9a48aa8b
xhasak <456221@mail.muni.cz>
2019-05-15 16:05:26 +0200

    added attributes from AppConfig

--- projects/kypo2-trainings-hurdling-viz-lib/src/lib/visualization/config/kypo2-trainings-hurdling-viz-lib.ts
+++ projects/kypo2-trainings-hurdling-viz-lib/src/lib/visualization/config/kypo2-trainings-hurdling-viz-lib.ts
@@ -1,3 +1,19 @@
+import {GenericObject} from '../models/generic-object.type';
+import {View} from '../models/view.enum';
+
 export class Kypo2TrainingsHurdlingVizLibConfig {
   kypo2TrainingsHurdlingRestBasePath: string;
+  apiUrl: string;
+  token: string; // temp
+  levelsTimePlan: number[];
+  gameColors: string[];
+  darkColor: string;
+  eventShapePaths: GenericObject;
+  minBarHeight: number;
+  maxBarHeight: number;
+  maxZoomValue: number;
+  zoomStep: number;
+  simulationInterval: number;
+  loadDataInterval: number;
+  defaultView: View;
 }
