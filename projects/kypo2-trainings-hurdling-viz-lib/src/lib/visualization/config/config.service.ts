758719cd859290af81842c7f8c971136a2170d6e
xhasak <456221@mail.muni.cz>
2019-05-15 13:51:33 +0200

    added attributes definitionId and gameId with getters and setters

--- projects/kypo2-trainings-hurdling-viz-lib/src/lib/visualization/config/config.service.ts
+++ projects/kypo2-trainings-hurdling-viz-lib/src/lib/visualization/config/config.service.ts
@@ -4,6 +4,24 @@ import {Kypo2TrainingsHurdlingVizLibConfig} from './kypo2-trainings-hurdling-viz
 @Injectable()
 export class ConfigService {
   private readonly _config: Kypo2TrainingsHurdlingVizLibConfig;
+  private _definitionId: number;
+  private _gameId: number;
+
+  get definitionId(): number {
+    return this._definitionId;
+  }
+
+  set definitionId(value: number) {
+    this._definitionId = value;
+  }
+
+  get gameId(): number {
+    return this._gameId;
+  }
+
+  set gameId(value: number) {
+    this._gameId = value;
+  }
 
   get config(): Kypo2TrainingsHurdlingVizLibConfig {
     return this._config;
