64afa8e7ca74c438931536df19fcb6b16d805712
xhasak <456221@mail.muni.cz>
2019-05-15 15:59:29 +0200

    added remaining attributes

--- src/app/custom-config.ts
+++ src/app/custom-config.ts
@@ -2,5 +2,18 @@ import {environment} from '../environments/environment';
 import {Kypo2TrainingsHurdlingVizLibConfig} from '../../projects/kypo2-trainings-hurdling-viz-lib/src/public_api';
 
 export const CustomConfig: Kypo2TrainingsHurdlingVizLibConfig = {
-  kypo2TrainingsHurdlingRestBasePath: environment.kypo2TrainingsHurdlingRestBasePath
+  kypo2TrainingsHurdlingRestBasePath: environment.kypo2TrainingsHurdlingRestBasePath,
+  apiUrl: environment.apiUrl,
+  token: environment.token, // temp
+  levelsTimePlan: environment.levelsTimePlan,
+  gameColors: environment.gameColors,
+  darkColor: environment.darkColor,
+  eventShapePaths: environment.eventShapePaths,
+  minBarHeight: environment.minBarHeight,
+  maxBarHeight: environment.maxBarHeight,
+  maxZoomValue: environment.maxZoomValue,
+  zoomStep: environment.zoomStep,
+  simulationInterval: environment.simulationInterval,
+  loadDataInterval: environment.loadDataInterval,
+  defaultView: environment.defaultView
 };
